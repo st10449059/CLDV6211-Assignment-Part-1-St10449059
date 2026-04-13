6b1bb715a6a0257bba7750490ee334608f62af7c
st10449059 <160851453+st10449059@users.noreply.github.com>
2026-04-13 23:19:20 +0200

    Improved the UI/UX and added tables to view up and coming events and people who booked

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,9 +9,13 @@ namespace CLDV6211_Assignment_Part_1_St10449059.Controllers
         public HomeController(ApplicationDbContext context) { _context = context; }
         public async Task<IActionResult> Index()
         {
+            // Fetch venues for the main model
             var venues = await _context.Venues.ToListAsync();
+
+            // Fetch events and bookings and store them in ViewBag
             ViewBag.Events = await _context.Events.Include(e => e.Venue).ToListAsync();
             ViewBag.Bookings = await _context.Bookings.Include(b => b.Event).ToListAsync();
+
             return View(venues);
         }
     }
